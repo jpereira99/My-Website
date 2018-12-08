925d9b1e59a529c87aa32ae96389462a60b53be9
Jayden <43273809+jpereira99@users.noreply.github.com>
2018-12-07 22:11:32 -0500

    webhook implementation v1.1

--- backend.py
+++ backend.py
@@ -1,27 +1,34 @@
 import json
 
 
-def jsonAppender(repoName, url, description, timeCreated):
+def jsonAppender(repoName, html, description, timeCreated):
 
+    #Reformatting Date RawData
+    timeYear = timeCreated[0:4]
+    timeMonth = timeCreated[5:7]
+    timeDay = timeCreated[8:10]
+
+    formattedTime = timeMonth + "/" + timeDay + "/" + timeYear
+
+    #Formatting pythonic dictionary to githubData.json format
     dataToAppend = [{
         "name" : repoName,
-        "url" : url,
+        "url" : html,
         "description" : description,
-        "created_at" : timeCreated
+        "created_at" : formattedTime
     }]
 
+    #Capturing old data
     with open("static/githubData.json") as dataViewer:
         oldData = json.load(dataViewer)
 
-    data = oldData + dataToAppend
+    #Checking if data is already present for repo
+    for n in oldData:
+        if dataToAppend[0]['name'] == n['name']:
+            break
+        else:
+            #Appending to database
+            data = oldData + dataToAppend
 
-
-    with open("static/githubData.json", "w") as writeToFile:
-        json.dump(data, writeToFile)
-
-def githubData():
-
-    with open("static/githubData.json") as dataViewer:
-        showcase = json.load(dataViewer)
-
-    return showcase
+            with open("static/githubData.json", "w") as writeToFile:
+                json.dump(data, writeToFile)
